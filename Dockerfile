b360115de6fc46aaa6bf18101405d7308d70d599
jimmyjongs <jdojimmy@gmail.com>
2021-04-06 15:33:42 -0700

    implement local backend api

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # The base go-image
-FROM golang:1.14-alpine
+FROM golang:1.16-alpine
  
 # Create a directory for the app
 RUN mkdir /app
--- main.go
+++ main.go
@@ -1,12 +1,95 @@
 package main
 
 import (
+	"app/models"
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/gorilla/mux"
 )
+
+var users []models.User
+
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	// set header
+	w.Header().Set("Content-Type", "application/json")
+
+	//encode users into json
+	json.NewEncoder(w).Encode(users)
+}
+
+func getUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	// get params
+	params := mux.Vars(r)
+
+	// loop thhru users, find with id
+	// for _ + for item in users
+	for _, item := range users {
+		if item.Username == params["username"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+
+	json.NewEncoder(w).Encode(&models.User{})
+
+}
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var user models.User
+	_ = json.NewDecoder(r.Body).Decode(&user)
+
+	users = append(users, user)
+	json.NewEncoder(w).Encode(user)
+
+}
+
+// // combintation of delete + create
+func updateUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	// get params
+	params := mux.Vars(r)
+
+	for index, item := range users {
+		if item.Username == params["username"] {
+			// delete's slice
+			users = append(users[:index], users[index+1:]...)
+
+			// create's creation
+			var user models.User
+			_ = json.NewDecoder(r.Body).Decode(&user)
+
+			user.Username = params["username"]
+			users = append(users, user)
+			json.NewEncoder(w).Encode(user)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(users)
+}
+
+func deleteUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	// get params
+	params := mux.Vars(r)
+
+	for index, item := range users {
+		if item.Username == params["username"] {
+			users = append(users[:index], users[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(users)
+}
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the HomePage!")
@@ -15,8 +98,34 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", homePage)
+	router := mux.NewRouter()
+
+	users = append(users, models.User{
+		Username:   "1",
+		Password:   "$5F3fAz1",
+		Firstname:  "John",
+		Lastname:   "Smith",
+		Middlename: "Kane",
+		Email:      "test@gmail.com",
+		Phone:      "123-456-7890",
+	})
+
+	users = append(users, models.User{
+		Username:   "2",
+		Password:   "$45zb34azza1",
+		Firstname:  "Jane",
+		Lastname:   "Doe",
+		Middlename: "Zae",
+		Email:      "test33@gmail.com",
+		Phone:      "000-000-7890",
+	})
+
+	router.HandleFunc("/api/users", getUsers).Methods("GET")
+	router.HandleFunc("/api/user/{username}", getUser).Methods("GET")
+	router.HandleFunc("/api/user", createUser).Methods("POST")
+	router.HandleFunc("/api/user/{username}", updateUser).Methods("PUT")
+	router.HandleFunc("/api/user/{username}", deleteUser).Methods("DELETE")
 
 	fmt.Println("Listening on port 8001...")
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
