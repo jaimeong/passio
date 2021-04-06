4ab8c55c94e913c91552777329c4d95644794527
jimmyjongs <jdojimmy@gmail.com>
2021-04-06 15:08:34 -0700

    implement user model

--- models/User.go
+++ models/User.go
@@ -1,1 +1,11 @@
 package models
+
+type User struct {
+	Username   string `json:"Username"`
+	Password   string `json:"Password"`
+	Firstname  string `json:"Firstname"`
+	Lastname   string `json:"Lastname"`
+	Middlename string `json:"Middlename"`
+	Email      string `json:"Email"`
+	Phone      string `json:"Phone"`
+}
